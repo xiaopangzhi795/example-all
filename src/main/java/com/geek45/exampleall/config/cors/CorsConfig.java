624d6aafbe35da2a5286047af454025651e85af2
rubik <qianzhilei@clbao.com>
2020-06-29 09:44:41 +0800

    解决跨域问题配置

---
+++ src/main/java/com/geek45/exampleall/config/cors/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.geek45.exampleall.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域问题的配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
---
+++ src/main/java/com/geek45/exampleall/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 项目中的一些配置类
+ */
+package com.geek45.exampleall.config;
